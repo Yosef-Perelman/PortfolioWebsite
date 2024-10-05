509f828da44bd849434fa817df6d3613be801b09
Yosef-Perelman <yosefper@gmail.com>
2024-10-05 22:00:57 +0300

    An arrangement for a few more points

--- src/app/page.js
+++ src/app/page.js
@@ -7,15 +7,14 @@ import Footer from "./components/Footer";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col container">
+    <main className="flex min-h-screen flex-col">
       <Navbar />
       <div className='container mt-24 mx-auto px-12 py-4'>
-      <OpenSection />
-      <AboutSection />
-      <ProjectsSection />
-      
+        <OpenSection />
+        <AboutSection />
+        <ProjectsSection />
       </div>
-      <Footer/>
+      <Footer />
     </main>
   );
 }
